99b2ea8b5f3bcac4b337106b630094432eeac915
Patrik Kopkan <pkopkan@redhat.com>
2019-10-11 19:03:22 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="specfile_generator",
+    version="0.0.0-dev",
+    author="Lumir Balhar",
+    author_email="author@example.com",
+    description="Project trying to automate making specfiles from pypi packages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/frenzymadness/specfile_generator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+    zip_safe=True,
+    license_files="LICENSE"
+)
+
